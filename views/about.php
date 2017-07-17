70c7a081e12f293a6abc83d4979e3c9db24d579c
Maks <lisnm@outlook.com>
2017-07-17 17:03:58 +0300

    Add files
    Добавление заготовок файлов.

---
+++ views/about.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html><html lang="ru">
+<head><meta charset="utf-8">
+     <title>О нас</title>
+    </head>
+    <body>
+<h3><?= $item->name; ?></h3>
+<h3><?= $item->description; ?></h3>
+    </body>
+</html>
