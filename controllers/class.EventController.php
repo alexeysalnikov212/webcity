537648f2fbd5dbd40e20488d47f1aebec1f85800
Lucky75 <1990-0@mail.ru>
2017-07-17 22:17:16 +0300

    Добавил комментарии.

--- controllers/class.EventController.php
+++ controllers/class.EventController.php
@@ -1,9 +1,11 @@
 <?php
-//класс контроллер событий
-//к нему обращается точка входа ( index.php), вызывая нужную функцию, 
-//сам контроллер обращается к моделям(models/class.Event),получает оттуда нужную информацию
-//и подключает view для вывода в html
-    class EventController extends AController
+/*
+класс контроллер событий наследуется от абстрактного
+к нему обращается точка входа ( index.php), вызывая нужную функцию, 
+сам контроллер обращается к моделям(models/class.Event),получает оттуда нужную информацию
+и подключает view для вывода в html
+*/
+class EventController extends AController
     {
         protected static $class = "Event";
         
--- models/class.Event.php
+++ models/class.Event.php
@@ -2,11 +2,9 @@
 //Класс событие, наследуется от абстрактной модели
 class Event extends AbstractModel
 {
-    
     protected static $table = "events";//для связи с базой из абстрактной модели
     protected static $class = "Event"; //для связи с базой из абстрактной модели
-    
-    
+        
     //свойства
     public $id;
     public $name;
