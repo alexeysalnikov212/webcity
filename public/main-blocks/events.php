688dd1ef39bde1a5cbf71738d78e186c1486082b
Maks <m.lisnyak@gmail.com>
2017-07-17 14:46:02 +0300

    Update views
    Перемещение папки Афиша -2 в public

---
+++ public/main-blocks/events.php
@@ -0,0 +1,180 @@
+<!-- Contents events -->
+
+ <div id="events">
+ <div class="main-text-body">
+
+
+    <h2>Самые интересные места отдыха в Краматорске ждут вас!</h2>
+
+
+
+  </div>
+
+
+  <div class="container-fluid ">
+    <div class="content">
+      <div class="row">
+        <div class="col-xs-6 col-sm-4 col-md-3">
+          <div class="thumbnail">
+            <div class="caption">
+              <div class="event-title">
+                <h2>Велопробег</h2>
+              </div>
+              <div class="event-description">
+                <p>Приглашаем принять участие в велопробеге...</p>
+              </div>
+              <div class="event-date">
+                <h3>24.07.2017</h3>
+              </div>
+              <p><a href="" class="label label-default">Узнать больше</a></p>
+            </div>
+            <div class="event-picture">
+              <div class="event-picture-wrap" style="background-image: url(img/Velosipedisty.jpg);"><img src="img/Velosipedisty.jpg" alt="" title=""></div>
+              </div>
+          </div>
+        </div>
+
+        <div class="col-xs-6 col-sm-4 col-md-3">
+          <div class="thumbnail">
+            <div class="caption">
+              <div class="event-title">
+                <h2>Thumbnail Headline</h2>
+              </div>
+              <div class="event-description">
+                <p>Приглашаем принять участие в велопробеге...</p>
+              </div>
+              <div class="event-date">
+                <h3>24.07.2017</h3>
+              </div>
+              <p><a href="" class="label label-default">Узнать больше</a></p>
+            </div>
+            <div class="event-picture">
+              <div class="event-picture-wrap" style="background-image: url(img/page3-img4.jpg);"><img src="img/page3-img4.jpg" alt="" title=""></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-6 col-sm-4 col-md-3">
+          <div class="thumbnail">
+            <div class="caption">
+              <div class="event-title">
+                <h2>Велопробег</h2>
+              </div>
+              <div class="event-description">
+                <p>Приглашаем принять участие в велопробеге...</p>
+              </div>
+              <div class="event-date">
+                <h3>24.07.2017</h3>
+              </div>
+              <p><a href="" class="label label-default">Узнать больше</a></p>
+            </div>
+            <div class="event-picture">
+              <div class="event-picture-wrap" style="background-image: url(img/page3-img6.jpg);"><img src="img/page3-img4.jpg" alt="" title=""></div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-6 col-sm-4 col-md-3">
+          <div class="thumbnail">
+            <div class="caption">
+              <div class="event-title">
+                <h2>Thumbnail Headline</h2>
+              </div>
+              <div class="event-description">
+                <p>Приглашаем принять участие в велопробеге...</p>
+              </div>
+              <div class="event-date">
+                <h3>24.07.2017</h3>
+              </div>
+              <p><a href="" class="label label-default">Узнать больше</a></p>
+            </div>
+            <div class="event-picture">
+              <div class="event-picture-wrap" style="background-image: url(img/page3-img4.jpg);"><img src="img/page3-img4.jpg" alt="" title=""></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+          <div class="row">
+            <div class="col-xs-6 col-sm-4 col-md-3">
+              <div class="thumbnail">
+                <div class="caption">
+                  <div class="event-title">
+                    <h2>Велопробег</h2>
+                  </div>
+                  <div class="event-description">
+                    <p>Приглашаем принять участие в велопробеге...</p>
+                  </div>
+                  <div class="event-date">
+                    <h3>24.07.2017</h3>
+                  </div>
+                  <p><a href="" class="label label-default">Узнать больше</a></p>
+                </div>
+                <div class="event-picture">
+                  <div class="event-picture-wrap" style="background-image: url(img/page3-img9.jpg);"><img src="img/page3-img4.jpg" alt="" title=""></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xs-6 col-sm-4 col-md-3">
+              <div class="thumbnail">
+                <div class="caption">
+                  <div class="event-title">
+                    <h2>Thumbnail Headline</h2>
+                  </div>
+                  <div class="event-description">
+                    <p>Приглашаем принять участие в велопробеге...</p>
+                  </div>
+                  <div class="event-date">
+                    <h3>24.07.2017</h3>
+                  </div>
+                  <p><a href="" class="label label-default">Узнать больше</a></p>
+                </div>
+                <div class="event-picture">
+                  <div class="event-picture-wrap" style="background-image: url(img/page3-img1.jpg);"><img src="img/page3-img1.jpg" alt="" title=""></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xs-6 col-sm-4 col-md-3">
+              <div class="thumbnail">
+                <div class="caption">
+                  <div class="event-title">
+                    <h2>Велопробег</h2>
+                  </div>
+                  <div class="event-description">
+                    <p>Приглашаем принять участие в велопробеге...</p>
+                  </div>
+                  <div class="event-date">
+                    <h3>24.07.2017</h3>
+                  </div>
+                  <p><a href="" class="label label-default">Узнать больше</a></p>
+                </div>
+                <div class="event-picture">
+                  <div class="event-picture-wrap" style="background-image: url(img/page3-img4.jpg);"><img src="img/page3-img4.jpg" alt="" title=""></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xs-6 col-sm-4 col-md-3">
+              <div class="thumbnail">
+                <div class="caption">
+                  <div class="event-title">
+                    <h2>Thumbnail Headline</h2>
+                  </div>
+                  <div class="event-description">
+                    <p>Приглашаем принять участие в велопробеге...</p>
+                  </div>
+                  <div class="event-date">
+                    <h3>24.07.2017</h3>
+                  </div>
+                  <p><a href="" class="label label-default">Узнать больше</a></p>
+                </div>
+                <div class="event-picture">
+                  <div class="event-picture-wrap" style="background-image: url(img/page3-img5.jpg);"><img src="img/page3-img4.jpg" alt="" title=""></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+          </div>
