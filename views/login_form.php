b6ffc5dee8ebe63e5bc0cc5d3cc5d1e555550832
Maks <m.lisnyak@gmail.com>
2017-07-21 08:52:24 +0300

    Перенос из public во views
    Весь html код идет в вид. Осталось навести там порядок:)))

--- views/login_form.php
+++ views/login_form.php
@@ -1,19 +1,32 @@
-<form action="login.php" method="post">
-    <fieldset>
-        <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="username" placeholder="Username" type="text"/>
-        </div>
-        <div class="form-group">
-            <input class="form-control" name="password" placeholder="Password" type="password"/>
-        </div>
-        <div class="form-group">
-            <button class="btn btn-default" type="submit">
-                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
-                Log In
-            </button>
-        </div>
-    </fieldset>
-</form>
-<div>
-    or <a href="register.php">register</a> for an account
+
+<div id="login">
+ <div class="col-md-12">
+  <div class="social-buttons">
+   <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+	<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
 </div>
+ или
+	 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8">
+										<div class="form-group">
+											 <label class="sr-only" for="email">Электронная почта</label>
+											 <input type="text" class="form-control" name="email" placeholder="Электронная почта" required>
+										</div>
+										<div class="form-group">
+											 <label class="sr-only" for="password">Пароль</label>
+											 <input type="password" class="form-control" name="password" placeholder="Пароль" required>
+                                             <div class="help-block text-right"><a href="forgotpass.php">Забыли пароль?</a></div>
+										</div>
+										<div class="form-group">
+											 <button type="submit" class="btn btn-primary btn-block">Войти</button>
+										</div>
+										<div class="checkbox">
+											 <label>
+											 <input type="checkbox"> Запомнить меня
+											 </label>
+										</div>
+								 </form>
+							</div>
+							<div class="bottom text-center">
+								Нет аккаунта ? <a href="register.php"><b>Регистрация</b></a>
+							</div>
+					 </div>
