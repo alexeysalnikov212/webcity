63884bb32ee2387321bcea669247e0e4839bd3d2
Maks <lisnm@outlook.com>
2017-07-27 18:34:10 +0300

    Проверка запросов на корректность

--- public/index.php
+++ public/index.php
@@ -7,8 +7,6 @@
     //Рендерить надо в контроллерах. точка доступа в идеале должна обращаться только к контроллерам.
     //render("main.php", ["title" => "main"]);
 
-?>
-<?php
 //подключение автозагрузки классов (ищет в папках classes,models,controllers)
 require_once __DIR__."/../includes/autoload.php";
 
@@ -22,9 +20,22 @@ $act = isset($_GET['act']) ? $_GET['act'] : 'Index';       // проверяем
 //это для того, чтоб в строке браузера при http://127.0.0.1/final/public/?ctrl=org&act=one&id=2
 //вызывало контроллер "организации(org)", который вызывает метод (actionOne) и берет id организации. 
 
-$className = $ctrl."Controller";   
-$controller = new $className;
+$className = $ctrl."Controller";
+// Проверяем существование класса перед его использованием
+if (class_exists($className)) {
+    $controller = new $className;
+}else
+{
+    render("404.php","404.php");
+}
+
+// Проверяем существование метода перед его использованием
 $method = 'action'.$act;
-$controller-> $method();
-?> 
+if(method_exists($controller, $method))
+{
+    $controller-> $method();
+}else
+{
+    render("404.php","404.php");
+}
 
