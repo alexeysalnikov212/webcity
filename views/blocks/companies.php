77ea43861c22f5a24b899429c0353257659754c1
Lucky75 <1990-0@mail.ru>
2017-07-29 22:49:02 +0300

    доубирал .jpg

--- views/blocks/companies.php
+++ views/blocks/companies.php
@@ -16,7 +16,7 @@
 
       	<a href="?ctrl=company&act=one&id=<?= $itemx->id; ?>" class="pull-left">
 
-    		<img class="picture-media" src="/../upload/<?= $itemx->picture_url; ?>.jpg">
+    		<img class="picture-media" src="/../upload/<?= $itemx->picture_url; ?>">
   		</a>
   		    <div class="company-name">
             
