6c4f67b999f99556e58d558ecaadd3b05cd8c71f
Lucky75 <1990-0@mail.ru>
2017-08-03 11:51:07 +0300

    описание поменял на название категории

--- views/blocks/company.php
+++ views/blocks/company.php
@@ -60,7 +60,7 @@
                                             <h2><?= $itemx->title; ?></h2>
                                         </div>
                                         <div class="event-description">
-                                            <p><?= $itemx->description; ?></p>
+                                            <p><?= $itemx->category_name->category_name; ?></p>
                                         </div>
                                         <div class="event-date">
                                             <h3><?= $itemx->date_start; ?></h3>
